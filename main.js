e6580c46a2642067afb3ccf62cbc4801da90c2cf
Roo_ba_h <120778806+SepehrSahali@users.noreply.github.com>
2023-01-24 12:26:48 +0330

    firstNav

---
+++ main.js
@@ -0,0 +1,11 @@
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    document.getElementById("openMenu").style.visibility='hidden'
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+    document.getElementById("openMenu").style.visibility='visible'
+  }
